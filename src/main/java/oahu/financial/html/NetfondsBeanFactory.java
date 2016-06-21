ba7b7dfc9f87ec3a6d7498417cdab87ad0b0b461
rcs <rolf.c.stadheim@gmail.com>
2016-06-21 15:38:46 +0200

    NetfondsBeanFactory.onDerivativeParsed

--- src/main/java/oahu/financial/html/NetfondsBeanFactory.java
+++ src/main/java/oahu/financial/html/NetfondsBeanFactory.java
@@ -1,8 +1,24 @@
 package oahu.financial.html;
 
+import java.time.LocalDate;
+
 /**
  * Created by rcs on 21.06.16.
  *
  */
 public interface NetfondsBeanFactory {
+    /*
+    private String optionName;
+    private String optionType;
+    private String xPrice;
+    private String expiry;
+    private String buy;
+    private String sell;
+    */
+    void onDerivativeParsed(String optionName,
+                            String optionType,
+                            double exercisePrice,
+                            LocalDate expiry,
+                            double buy,
+                            double sell);
 }
