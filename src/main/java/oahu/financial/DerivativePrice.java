dbb02b1144b0934c96f103b20b2466e97a61a7bf
Rolf C Stadheim <rolf.c.stadheim@gmail.com>
2017-06-07 21:54:32 +0200

    get_current.. -> getCurrentRiscOptionValue

--- src/main/java/oahu/financial/DerivativePrice.java
+++ src/main/java/oahu/financial/DerivativePrice.java
@@ -22,6 +22,6 @@ public interface DerivativePrice {
     int getOid();
     void setOid(int oid);
     String getTicker();
-    public double get_currentRiscOptionValue();
+    public double getCurrentRiscOptionValue();
     public Optional<Double> getCurrentRisc();
 }
